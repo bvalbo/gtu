0d16934136c93c5f284f5ecb8df8ae9ea713fa80
Bjørnar Valbø <bvalbo@gmail.com>
2025-05-18 20:04:45 +0200

    fix: Update results template to correct position for 'Hans Patrik' and add 'Jon Terje'

--- lib/constants.ts
+++ lib/constants.ts
@@ -59,7 +59,8 @@ export const RESULTS_TEMPLATE = [
   { position: 5, name: 'Finn Oscar', time: '6:14:25', notes: '' },
   { position: 6, name: 'Sondre', time: '6:47:20', notes: '' },
   { position: 6, name: 'Bjørn Olav', time: '6:47:20', notes: '' },
-  { position: 7, name: 'Hans Patrik', time: '7:00:55', notes: '' }
+  { position: 8, name: 'Hans Patrik', time: '7:00:55', notes: '' },
+  { position: 9, name: 'Jon Terje', time: '7:03:05', notes: '' }
 ];
 
 // Eksempel på live-tracking-linker (kan oppdateres før løpet)
